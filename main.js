23f54217a17818840a59109d5af4c4935ae8fa19
rayhanalshorif133 <rayhanalshorif.edu2@gmail.com>
2023-04-05 14:03:00 +0600

    callout-title seted colored

--- main.js
+++ main.js
@@ -5,15 +5,6 @@ $(function () {
 const sideBarClickHandler = () => {
     $(".color-item").on("click",function(){
        let class_title =  $(this).find('.color-item--title').text();
-       let classLists = $(".callout-title").prop('classList');
-        console.log(classLists.length);
-        for (let i = 0; i < classLists.length; i++) {
-            const className = classLists[i].className;
-            console.log(ok);
-        }
-        // classLists.map(function(class_list){
-        //     console.log(class_list);
-        // });
-       $(".callout-title").addClass(class_title);
+        $(".callout-title").removeClass().addClass('callout-title color__colored').addClass(class_title);
     });
 }
