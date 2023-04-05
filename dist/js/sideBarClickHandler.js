35318ba94523154135ebcff3cb5cbfe8bed2ab8a
rayhanalshorif133 <rayhanalshorif.edu2@gmail.com>
2023-04-05 16:03:56 +0600

    randomBGColorHandler

--- dist/js/sideBarClickHandler.js
+++ dist/js/sideBarClickHandler.js
@@ -13,13 +13,15 @@ const sideBarClickHandler = () => {
     });
 
     $(".color-item-random").on("click", function () {
-        bg_class_title = $(this).find('.color-item--title').text();
-        $(".callout-title").removeClass().addClass('callout-title color__colored').addClass(bg_class_title);
-        $(".callout-subtitle").removeClass().addClass('callout-subtitle color__colored').addClass(bg_class_title);
-        $(".callout-showList").removeClass().addClass('callout-showList color__colored').addClass(bg_class_title);
+        text_class_title = $(this).find('.color-item--title').text();
+        $(".callout-title").removeClass().addClass('callout-title color__colored').addClass(text_class_title);
+        $(".callout-subtitle").removeClass().addClass('callout-subtitle color__colored').addClass(text_class_title);
+        $(".callout-showList").removeClass().addClass('callout-showList color__colored').addClass(text_class_title);
     });
-
-
+    $(".color-item-random-bg").on("click", function () {
+        bg_class_title = $(this).find('.color-item--title').text();
+        $(".intro").removeClass().addClass('intro color__colored').addClass(bg_class_title);
+    });
 }
 
 export default sideBarClickHandler;
