c4dd710834a5af4ac08d708b7719c1bea4402e6b
rayhanalshorif133 <rayhanalshorif.edu2@gmail.com>
2023-04-05 16:00:53 +0600

    animate side

--- dist/js/sideBarClickHandler.js
+++ dist/js/sideBarClickHandler.js
@@ -10,9 +10,16 @@ const sideBarClickHandler = () => {
     $(".color-item-bg").on("click", function () {
         bg_class_title = $(this).find('.color-item--title').text();
         $(".intro").removeClass().addClass('intro color__colored').addClass(bg_class_title);
-        // $(".callout-subtitle").removeClass().addClass('callout-subtitle color__colored').addClass(text_class_title);
-        // $(".callout-showList").removeClass().addClass('callout-showList color__colored').addClass(text_class_title);
     });
+
+    $(".color-item-random").on("click", function () {
+        bg_class_title = $(this).find('.color-item--title').text();
+        $(".callout-title").removeClass().addClass('callout-title color__colored').addClass(bg_class_title);
+        $(".callout-subtitle").removeClass().addClass('callout-subtitle color__colored').addClass(bg_class_title);
+        $(".callout-showList").removeClass().addClass('callout-showList color__colored').addClass(bg_class_title);
+    });
+
+
 }
 
 export default sideBarClickHandler;
